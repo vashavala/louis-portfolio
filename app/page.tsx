b05c9eb7435de881a92f99b929162deb0ba98607
vasha <vashavala@outlook.com>
2024-03-04 13:20:25 +0800

    selfie

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen min-w-screen flex flex-col items-center justify-center">
-      <div className="Hero text-[#fff] font-black text-9xl">
-        Hello Visitor
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="hero min-h-screen min-w-screen bg-[url('/img/hero-pattern.svg')]">
+
       </div>
+      {/* <Image
+        src="/selfie.jpg"
+        alt="selfie"
+        width={400}
+        height={400}
+        priority
+      /> */}
     </main>
   );
 }

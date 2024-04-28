192cb4b4bb482ef3e72a1cb6fd6e17af3aace3b5
vasha <vashavala@outlook.com>
2024-04-28 23:50:59 +0800

    cvGarage

--- app/components/hero/page.tsx
+++ app/components/hero/page.tsx
@@ -1,13 +1,13 @@
 export default function Hero() {
   return (
     <section className="min-h-screen min-w-full flex items-center justify-center gap-20" id="Hero">
-      <div className="flex flex-col gap-2">
-        <div className="text-7xl font-black mb-10">FrontEnd Developer</div>
+      <div className="w-64 aspect-square bg-[url('/selfie.jpg')] bg-no-repeat bg-contain rounded-full"></div>
+      <div className="text-7xl font-black mb-10">Software developer</div>
+      <div>
         <div className="text-2xl">+ 5 years experience</div>
         <div className="text-2xl">+ reliable, conscientious, efficient </div>
         <div className="text-2xl">+ performance optimization, clean code, scalability </div>
       </div>
-      <div className="w-64 aspect-square bg-[url('/selfie.jpg')] bg-no-repeat bg-contain rounded-full"></div>
     </section>
   );
 }
